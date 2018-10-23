3ad3805fcdb24177c8aff28b2d227f54188a23dd
innofactor-vmy <44393724+innofactor-vmy@users.noreply.github.com>
2018-10-23 13:52:27 +0300

    Datamanager and CabinController

---
+++ Components/DataManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace vuets
+{
+    public class DataManager
+    {
+        const string dbFolder = @"C:\Database\";
+
+        public void Save(Cabin cabin)
+        {
+            var jsonCabin = JsonConvert.SerializeObject(cabin);
+            File.WriteAllText(dbFolder + cabin.ID, jsonCabin);
+        }
+
+        public List<Cabin> Load()
+        {
+            var cabins = new List<Cabin>();
+            var listOfFiles = Directory.EnumerateFiles(dbFolder);
+            foreach (var file in listOfFiles)
+            {
+                var text = File.ReadAllText(file);
+                var cabin = JsonConvert.DeserializeObject<Cabin>(text);
+                cabins.Add(cabin);
+            }
+
+            return cabins;
+        }
+    }
+}
---
+++ Controllers/CabinController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace vuets.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cabin")]
+    public class CabinController : Controller
+    {
+        // GET: api/Cabin
+        [HttpGet]
+        public string Get()
+        {
+            var db = new DataManager();
+            var cabins = db.Load();
+            return JsonConvert.SerializeObject(cabins);
+        }
+
+        // GET: api/Cabin/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Cabin
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Cabin/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
