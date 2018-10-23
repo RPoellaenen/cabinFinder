1dd37c074d5cc376e5faefe3a755b155471cb5bd
innofactor-terhikki <44394405+innofactor-terhikki@users.noreply.github.com>
2018-10-23 15:08:39 +0300

    Create

---
+++ ClientApp/components/home/home.ts
@@ -0,0 +1,14 @@
+﻿
+
+@Component
+export default class FetchDataComponent extends Vue {
+    forecasts: WeatherForecast[] = [];
+
+    mounted() {
+        fetch('api/SampleData/WeatherForecasts')
+            .then(response => response.json() as Promise<WeatherForecast[]>)
+            .then(data => {
+                this.forecasts = data;
+            });
+    }
+}
