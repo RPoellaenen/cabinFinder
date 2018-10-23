b86b245bbb79a74943fa6e427c614ba4172bd67d
R.Pöllänen <riku.pollanen@gmail.com>
2018-10-23 16:29:25 +0300

    Created cabin listing

--- ClientApp/components/home/home.ts
+++ ClientApp/components/home/home.ts
@@ -1,6 +1,16 @@
-﻿
+﻿import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { Cabin } from '../../cabin/cabin';
+import * as $ from 'jquery';
 
-//@Component
-//export default class FetchDataComponent extends Vue {
-
-//}
+@Component
+export default class HomeComponent extends Vue {
+    cabins: Cabin[] = [];
+    mounted() {
+        fetch('api/cabin')
+            .then(response => response.json() as Promise<Cabin[]>)
+            .then(data => {
+                this.cabins = data;
+            });
+    }
+}
